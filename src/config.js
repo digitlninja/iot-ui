7c9fa3671ee1902ff80eaeabe9c92f8cfe438a36
william <williamlangroudi@gmail.com>
2020-07-31 13:18:34 +0200

    - Remove unused config

--- src/config.js
+++ src/config.js
@@ -1,15 +1,4 @@
 export const API_URI =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3001/graphql"
-    : process.env.GRAPHQL_API_URI;
-
-export const UI_APP_URI =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : process.env.APP_URI;
-
-export const tokenTypes = {
-  idToken: "idToken",
-  accessToken: "accessToken",
-  refreshToken: "refreshToken",
-};
+    : process.env.REACT_APP_GRAPHQL_API_URI;
